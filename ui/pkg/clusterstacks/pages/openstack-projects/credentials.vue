39f0cf6d58ae1330c0be4ff76fb90108a3617d1d
Axel Schmidt <axel.schmidt@rz.uni-augsburg.de>
2026-03-30 15:44:39 +0200

    fix routes

---
+++ ui/pkg/clusterstacks/pages/openstack-projects/credentials.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <OpenstackCredentialsList />
+  </div>
+</template>
+
+<script>
+import OpenstackCredentialsList from './index.vue';
+export default {
+  components: { OpenstackCredentialsList }
+};
+</script>
